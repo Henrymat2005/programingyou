9905197a35c4fd5bfbc903eb6bf2310459dc3d44
Henrymat2005 <83827167+Henrymat2005@users.noreply.github.com>
2021-05-07 10:04:06 -0500

    Update formulario.php

--- formulario.php
+++ formulario.php
@@ -5,9 +5,8 @@
   $correo = $_POST["correo"];
   $mensaje = $_POST["mensaje"];
   
-  &header = "Enviado desde Programing You";
-  mensajeCompleto = $mensaje . "\nAtentamente: " . $nombre;
-  mail($destino, $asunto, $mensaje, $header);
+  $mensajeCompleto = $mensaje . "\nAtentamente: " . $nombre;
+  mail($destino, $asunto, $mensaje);
   echo "<script>alert('Correo enviado Existosamente')</script>";
   echo "<script>setTimeout(\"Location.href='index.html'\",1000)</script>";
 ?>
